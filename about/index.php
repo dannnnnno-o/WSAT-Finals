be4ab4c28d32e910552fb00918db44bb7794b298
dannnnnno-o <dannnnnnnno.o@gmail.com>
2026-04-05 18:03:34 +0800

    responsiveness on about page

--- about/index.php
+++ about/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+<<<<<<< Updated upstream
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Breeds of Cats</title>
@@ -9,6 +10,14 @@
     <link rel="stylesheet" href="style.css">
     <link rel="icon" href="/images/bg/catty.png">
 
+=======
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Breeds of Cats</title>
+<link rel="stylesheet" href="../style.css">
+<link rel="stylesheet" href="style.css">
+<link rel="icon" href="/images/bg/catty.png">
+>>>>>>> Stashed changes
 </head>
 
 <body>
@@ -38,6 +47,7 @@
 
     </header>
 
+<<<<<<< Updated upstream
     <section class="all">
         <center>
             <h2>Learn About Cat Breeds Around the World</h2><br>
@@ -45,6 +55,13 @@
                 them.<br>
                 This website was created to bring together helpful, well-organized, <br>
                 and easy-to-understand information about cat breeds from all over the world.<br><br>
+=======
+<section class="container">
+    <h2>Learn About Cat Breeds Around the World</h2><br>
+        <p>Welcome to Popular Cat Breeds, a dedicated space for anyone who loves cats or wants to learn more about them.<br> 
+            This website was created to bring together helpful, well-organized, <br>
+            and easy-to-understand information about cat breeds from all over the world.<br><br>
+>>>>>>> Stashed changes
 
                 Cats have been companions to humans for thousands of years,
                 yet each breed continues to offer something unique.<br>
@@ -67,6 +84,7 @@
                 Whether you are a first-time pet owner or someone with experience, our guides are here to support
                 you.<br><br>
 
+<<<<<<< Updated upstream
                 This website is built with passion and genuine interest in feline companions. <br>
                 Every section is thoughtfully created to make learning simple, enjoyable, and meaningful. <br>
                 We aim to create a space where curiosity about cats turns into knowledge, and knowledge turns into
@@ -89,6 +107,25 @@
                 By simplifying complex details and presenting them in an organized way, <br>
                 we aim to make learning about cats both enjoyable and practical.</p>
         </center>
+=======
+            This website is built with passion and genuine interest in feline companions. <br>
+            Every section is thoughtfully created to make learning simple, enjoyable, and meaningful. <br>
+            We aim to create a space where curiosity about cats turns into knowledge, and knowledge turns into better care.<br>
+        </p>
+
+
+
+    <br><br>
+    <h2> Mission </h2>
+        <p>Our mission is to educate and inspire people by providing accurate, clear, <br>
+            and engaging information about cat breeds and proper cat care. <br>
+            We strive to help individuals make informed decisions when choosing and raising a cat, <br>
+            promoting responsible pet ownership at all times.<br><br>
+
+            We are committed to making information easy to understand, especially for students, beginners, and casual readers. <br>
+            By simplifying complex details and presenting them in an organized way, <br>
+            we aim to make learning about cats both enjoyable and practical.</p>
+>>>>>>> Stashed changes
 
     </section>
     <footer>
