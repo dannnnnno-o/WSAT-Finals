593f663a01e280a000543c5af839f3c714757c7d
dannnnnno-o <dannnnnnnno.o@gmail.com>
2026-04-05 23:20:29 +0800

    merged with breeds background

--- breeds/index.php
+++ breeds/index.php
@@ -21,6 +21,13 @@ require_once('../ctrl.php');
 <body>
 
 
+<div class="bubble" style="width: 80px; height: 80px; left: 10%; animation-duration: 30s;"></div>
+  <div class="bubble" style="width: 50px; height: 50px; left: 40%; animation-duration: 20s;"></div>
+  <div class="bubble" style="width: 100px; height: 100px; left: 70%; animation-duration: 40s;"></div>
+
+  <div class="sparkle" style="top: 20%; left: 15%; animation-delay: 0s;"></div>
+  <div class="sparkle" style="top: 40%; left: 50%; animation-delay: 1s;"></div>
+  <div class="sparkle" style="top: 70%; left: 30%; animation-delay: 2s;"></div>
 
     <header>
         <a href="../" class="logo">
@@ -52,229 +59,160 @@ require_once('../ctrl.php');
 
         <?php
         if (empty($_GET["search"])) {
-            echo '
-<div class="container swiper">
-        <div class="wrapper">
-        <div class="card-list swiper-wrapper">
-        <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../images/cats/bengal-cat.jpg" alt="Bengal">
-        <p class="card-tag">Bengal</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About Bengal Cats</h3>
-        <p class="card-text">Popular for their wild-looking spotted coat and energetic, playful nature.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../images/cats/bengal-cat.jpg" alt="Bengal">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Bengal</span>
-        <span class="card-profile-role">Wild Adventurer</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Bengal Cat" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
-          <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../images/cats/mainecoon.jpg" alt="Mainecoon">
-        <p class="card-tag">Mainecoon</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About Maine Coon Cats</h3>
-        <p class="card-text">Famous for their large size, bushy tail, and gentle “gentle giant” personality.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../images/cats/mainecoon.jpg" alt="Mainecoon">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Maine Coon</span>
-        <span class="card-profile-role">The Gentle Giant</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Maine Coon" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
-          <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../images/cats/Persian cat.jpg" alt="Persian">
-        <p class="card-tag">Persian</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About Persian Cats</h3>
-        <p class="card-text">Known for their long, fluffy fur and calm, sweet temperament.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../images/cats/Persian cat.jpg" alt="Persian">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Persian</span>
-        <span class="card-profile-role">Elegant Aristocrat</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Persian Cat" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
+            echo '        <div class="wrapper swiper">
+            <div class="card-list swiper-wrapper">
+                <div class="card swiper-slide">
+                    <div class="card-image">
+                        <img src="../images/cats/bengal-cat.jpg" alt="Bengal">
+                        <p class="card-tag">Bengal</p>
+                    </div>
+                    <div class="card-content">
+                        <h3 class="card-title">All About Bengal Cats</h3>
+                        <p class="card-text">Popular for their wild-looking spotted coat and energetic, playful nature.</p>
+                        <div class="card-footer">
+                            <div class="card-profile">
+                                <img src="../images/cats/bengal-cat.jpg" alt="Bengal">
+                                <div class="card-profile-info">
+                                    <span class="card-profile-name">Bengal</span>
+                                    <span class="card-profile-role">Wild Adventurer</span>
+                                </div>
+                            </div>
+                            <a href="../cats/?name=Bengal Cat" class="card-button">Know More</a>
+                        </div>
+                    </div>
+                </div>
 
-         <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../10popular/ragdoll.jpg" alt="Ragdoll">
-        <p class="card-tag">Ragdoll</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About Ragdoll Cats</h3>
-        <p class="card-text">Known for their blue eyes and tendency to go limp when held; very affectionate.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../10popular/ragdoll.jpg" alt="Ragdoll">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Ragdoll</span>
-        <span class="card-profile-role">Loving Companion</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Ragdoll" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
+                <div class="card swiper-slide">
+                    <div class="card-image">
+                        <img src="../images/cats/mainecoon.jpg" alt="Maine Coon">
+                        <p class="card-tag">Maine Coon</p>
+                    </div>
+                    <div class="card-content">
+                        <h3 class="card-title">All About Maine Coon Cats</h3>
+                        <p class="card-text">Famous for their large size, bushy tail, and gentle "gentle giant" personality.</p>
+                        <div class="card-footer">
+                            <div class="card-profile">
+                                <img src="../images/cats/mainecoon.jpg" alt="Maine Coon">
+                                <div class="card-profile-info">
+                                    <span class="card-profile-name">Maine Coon</span>
+                                    <span class="card-profile-role">The Gentle Giant</span>
+                                </div>
+                            </div>
+                            <a href="../cats/?name=Maine Coon" class="card-button">Know More</a>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="card swiper-slide">
+                    <div class="card-image">
+                        <img src="../images/cats/Persian cat.jpg" alt="Persian">
+                        <p class="card-tag">Persian</p>
+                    </div>
+                    <div class="card-content">
+                        <h3 class="card-title">All About Persian Cats</h3>
+                        <p class="card-text">Known for their long, fluffy fur and calm, sweet temperament.</p>
+                        <div class="card-footer">
+                            <div class="card-profile">
+                                <img src="../images/cats/Persian cat.jpg" alt="Persian">
+                                <div class="card-profile-info">
+                                    <span class="card-profile-name">Persian</span>
+                                    <span class="card-profile-role">Elegant Aristocrat</span>
+                                </div>
+                            </div>
+                            <a href="../cats/?name=Persian Cat" class="card-button">Know More</a>
+                        </div>
+                    </div>
+                </div>
 
-          <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../10popular/Abyssinian-cat.jpg" alt="Abyssinian">
-        <p class="card-tag">Abyssinian</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About Abyssinian Cats</h3>
-        <p class="card-text">Known for their short ticked coat, active and curious personality.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../10popular/Abyssinian-cat.jpg" alt="Abyssinian">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Abyssinian</span>
-        <span class="card-profile-role">Curious Explorer</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Abyssinian Cat" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
+                <div class="card swiper-slide">
+                    <div class="card-image">
+                        <img src="../10popular/ragdoll.jpg" alt="Ragdoll">
+                        <p class="card-tag">Ragdoll</p>
+                    </div>
+                    <div class="card-content">
+                        <h3 class="card-title">All About Ragdoll Cats</h3>
+                        <p class="card-text">Known for their blue eyes and tendency to go limp when held; very affectionate.</p>
+                        <div class="card-footer">
+                            <div class="card-profile">
+                                <img src="../10popular/ragdoll.jpg" alt="Ragdoll">
+                                <div class="card-profile-info">
+                                    <span class="card-profile-name">Ragdoll</span>
+                                    <span class="card-profile-role">Loving Companion</span>
+                                </div>
+                            </div>
+                            <a href="../cats/?name=Ragdoll" class="card-button">Know More</a>
+                        </div>
+                    </div>
+                </div>
 
-          <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../10popular/Birman.webp" alt="Birman">
-        <p class="card-tag">Birman</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About Birman Cats</h3>
-        <p class="card-text">Recognized for their striking blue eyes, silky coat, and gentle temperament.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../10popular/Birman.webp" alt="Birman">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Birman</span>
-        <span class="card-profile-role">Sacred Beauty</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Birman" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
+                <div class="card swiper-slide">
+                    <div class="card-image">
+                        <img src="../10popular/Abyssinian-cat.jpg" alt="Abyssinian">
+                        <p class="card-tag">Abyssinian</p>
+                    </div>
+                    <div class="card-content">
+                        <h3 class="card-title">All About Abyssinian Cats</h3>
+                        <p class="card-text">Known for their short ticked coat, active and curious personality.</p>
+                        <div class="card-footer">
+                            <div class="card-profile">
+                                <img src="../10popular/Abyssinian-cat.jpg" alt="Abyssinian">
+                                <div class="card-profile-info">
+                                    <span class="card-profile-name">Abyssinian</span>
+                                    <span class="card-profile-role">Curious Explorer</span>
+                                </div>
+                            </div>
+                            <a href="../cats/?name=Abyssinian Cat" class="card-button">Know More</a>
+                        </div>
+                    </div>
+                </div>
 
-          <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../10popular/british.jpg" alt="British">
-        <p class="card-tag">British Shorthair</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About British Shorthair Cats</h3>
-        <p class="card-text">Loved for their round face, dense coat, and easygoing temperament.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../10popular/british.jpg" alt="British Shorthair">
-        <div class="card-profile-info">
-        <span class="card-profile-name">British Shorthair</span>
-        <span class="card-profile-role">Calm Gentleman</span>
-        </div>
-        </div>
-        <a href="../cats/?name=British Shorthair" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
+                <div class="card swiper-slide">
+                    <div class="card-image">
+                        <img src="../10popular/Birman.webp" alt="Birman">
+                        <p class="card-tag">Birman</p>
+                    </div>
+                    <div class="card-content">
+                        <h3 class="card-title">All About Birman Cats</h3>
+                        <p class="card-text">Recognized for their striking blue eyes, silky coat, and gentle temperament.</p>
+                        <div class="card-footer">
+                            <div class="card-profile">
+                                <img src="../10popular/Birman.webp" alt="Birman">
+                                <div class="card-profile-info">
+                                    <span class="card-profile-name">Birman</span>
+                                    <span class="card-profile-role">Sacred Beauty</span>
+                                </div>
+                            </div>
+                            <a href="../cats/?name=Birman" class="card-button">Know More</a>
+                        </div>
+                    </div>
+                </div>
 
-        
-         <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../10popular/Scottish Fold.jpg" alt="Scottish Fold">
-        <p class="card-tag">Scottish Fold</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About Scottish Fold Cats</h3>
-        <p class="card-text">Famous for their folded ears, round face, and calm nature.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../10popular/Scottish Fold.jpg" alt="Scottish Fold">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Scottish Fold</span>
-        <span class="card-profile-role">The Sweet Owl</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Scottish Fold" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
+                <div class="card swiper-slide">
+                    <div class="card-image">
+                        <img src="../10popular/british.jpg" alt="British Shorthair">
+                        <p class="card-tag">British Shorthair</p>
+                    </div>
+                    <div class="card-content">
+                        <h3 class="card-title">All About British Shorthair Cats</h3>
+                        <p class="card-text">Loved for their round face, dense coat, and easygoing temperament.</p>
+                        <div class="card-footer">
+                            <div class="card-profile">
+                                <img src="../10popular/british.jpg" alt="British Shorthair">
+                                <div class="card-profile-info">
+                                    <span class="card-profile-name">British Shorthair</span>
+                                    <span class="card-profile-role">Calm Gentleman</span>
+                                </div>
+                            </div>
+                            <a href="../cats/?name=British Shorthair" class="card-button">Know More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-
-        <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../10popular/siamese.jpg" alt="Siamese">
-        <p class="card-tag">Siamese</p>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
         </div>
-        <div class="card-content">
-        <h3 class="card-title">All About  Siamese Cats</h3>
-        <p class="card-text">Famous for their color-point coat, striking blue eyes, and vocal, social personality.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../10popular/siamese.jpg" alt="Siamese">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Siamese</span>
-        <span class="card-profile-role">Talkative Royal</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Siamese Cat" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
-
-  <div class="card swiper-slide">
-        <div class="card-image">
-        <img src="../10popular/sphynx.jpg" alt="Sphynx">
-        <p class="card-tag">Sphynx</p>
-        </div>
-        <div class="card-content">
-        <h3 class="card-title">All About Sphynx Cats</h3>
-        <p class="card-text">Recognized for their hairless body, affectionate personality, and striking appearance.</p>
-        <div class="card-footer">
-        <div class="card-profile">
-        <img src="../10popular/Sphynx.jpg" alt="Sphynx">
-        <div class="card-profile-info">
-        <span class="card-profile-name">Sphynx</span>
-        <span class="card-profile-role">Affectionate Alien</span>
-        </div>
-        </div>
-        <a href="../cats/?name=Sphynx Cat" class="card-button">Know More</a>
-        </div>
-        </div>
-        </div>
-        </div>
-
-  <div class="swiper-pagination"></div>
-
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
-
-        </div>
-</div>
 ';
         }
 
